467aa3ada495ffb18368e9b69290213b948ad792
iCiaran <iCiaran@users.noreply.github.com>
2017-08-17 22:48:10 +0100

    Use config file

--- week3/main.py
+++ week3/main.py
@@ -5,14 +5,18 @@ from math import sin,cos
 from sys import exit
 
 def main():
-    width = 60
-    height = 60
-    border = 10
-    length = 8
-    difficulty = 10
-    solve = False
+    config = load_config()
+    print(config)
+    width = int(config[0])
+    height = int(config[1])
+    border = int(config[2])
+    length = int(config[3])
+    difficulty = max(min(int(config[4]),100),0)
+    solve = config[5] == 1
+    gen_type = int(config[6])
+    file = config[7]
 
-    mask = Image.open("rabbit.jpg").convert("RGB")
+    mask = Image.open(file).convert("RGB")
     width,height = mask.size
     image = Image.new("RGB", (width*length + 2*border,height*length + 2*border),
                       color="#0D0221")
@@ -27,7 +31,7 @@ def main():
            lambda c: (c[0] * c[1]) % 12 + c[0] + random()*1.5,
            lambda c: c[1] if get_brightness(mask, c) < 0.5 else c[0]*random()]
     
-    maze = Maze(width, height, gen[1])
+    maze = Maze(width, height, gen[gen_type])
     print("generated")
     maze.draw(drawer, length, border)
     image.save("maze.png", "png")
@@ -38,6 +42,10 @@ def main():
         maze.draw_path(path, drawer, length, border)
         image.save("maze_solved.png", "png")
         print("saved")
+
+def load_config(file="default.conf"):
+    return [line.split("=")[1].strip() for line in open(file)]
+
 
 def get_brightness(i, xy):
     r,g,b = i.getpixel(xy)
