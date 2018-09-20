eba0d9dbb08ddcc563a4bbebc31654d9d50d396d
iCiaran <iCiaran@users.noreply.github.com>
2018-09-21 00:15:09 +0100

    Start implementing median of five method
    Still need to implement median_five

--- 26-09/main.cpp
+++ 26-09/main.cpp
@@ -5,6 +5,13 @@
 #include <iterator>
 #include <algorithm>
 
+std::vector<int> split(const std::string& s);
+int partition(std::vector<int>& list, int left, int right, int pivotIndex);
+int median_five(std::vector<int>& list, int left, int right);
+int pivot(std::vector<int>& list, int left, int right);
+int select(std::vector<int>& list, int left, int right, int k);
+int main(int argc, char **argv);
+
 std::vector<int> split(const std::string& s) {
     std::istringstream iss{s};
     return std::vector<int>{std::istream_iterator<int>{iss}, {}};
@@ -25,22 +32,54 @@ int partition(std::vector<int>& list, int left, int right, int pivotIndex){
     return storeIndex;    
 }
 
-int select(std::vector<int>& list, int left, int right, int k){
-    if(left == right) {
-        return list[left];
-    }
-    int pivotIndex = (left + right) / 2;
-    pivotIndex = partition(list, left, right, pivotIndex);
-    if(k == pivotIndex) {
-        return list[k];
-    } else if (k < pivotIndex) {
-        return select(list, left, pivotIndex - 1, k);
-    } else {
-        return select(list, pivotIndex + 1, right, k);
-    }
+int median_five(std::vector<int>& list, int left, int right) {
+    return 1;
 }
 
-int main(int argc, char *argv[]){
+int pivot(std::vector<int>& list, int left, int right) {
+    int subRight, median;
+    
+    if ((right - left) < 5) return median_five(list, left, right);
+    for (int i = left; i < right; i += 5) {
+        subRight = i + 4;
+        if (subRight > right) {
+            subRight = right;
+        }
+
+        median = median_five(list, i, subRight);
+        std::swap(list[subRight], list[left + (i - left) / 5]);
+    }
+    return select(list, left, left + (right - left)/5, (right-left)/10 + 1);
+}
+
+int select(std::vector<int>& list, int left, int right, int k){
+    int pivotIndex;
+    while (true) {
+        if (left == right) return list[left];
+        pivotIndex = partition(list, left, right, pivot(list, left, right));
+        if (pivotIndex == k) {
+            return list[k];
+        } else if (k < pivotIndex) {
+            right = pivotIndex - 1;
+        } else {
+            left = pivotIndex + 1;
+        }
+    }
+    // if(left == right) {
+    //     return list[left];
+    // }
+    // int pivotIndex = (left + right) / 2;
+    // pivotIndex = partition(list, left, right, pivotIndex);
+    // if(k == pivotIndex) {
+    //     return list[k];
+    // } else if (k < pivotIndex) {
+    //     return select(list, left, pivotIndex - 1, k);
+    // } else {
+    //     return select(list, pivotIndex + 1, right, k);
+    // }
+}
+
+int main(int argc, char **argv){
     if(argc != 3) {
         std::cout << "Usage: " << argv[0] << " filename k\n";
         return 1;
